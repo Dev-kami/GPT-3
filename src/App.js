4368437b3e5868f8f10a19f6c907395c8bd3aa1e
Obasi Chizitere <blossomobasi2@gmail.com>
2024-01-14 16:54:04 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// ------- Components ---------
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import CompaniesLogo from "./components/CompaniesLogo";
+import About from "./components/About";
+import Feature1 from "./components/Feature1";
+import Feature2 from "./components/Feature2";
+import Cta from "./components/Cta";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
+      <main>
+        <Home />
+      </main>
+      <CompaniesLogo />
+      <About />
+      <Feature1 />
+      <Feature2 />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 }
